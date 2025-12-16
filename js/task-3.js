8df75b4f28899e37e57a3be47ee02a411c69adb2
specua <kalashnikov.kiril8@gmail.com>
2025-12-16 16:55:35 +0200

    bug fix (task-1,3)

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return (content + (padding * 2) + (border * 2));
+    return ((content) + (padding + border) * 2);
 } 
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
